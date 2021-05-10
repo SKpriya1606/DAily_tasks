e031709ea3bc24a431b41846aebe3747e808529f
SKpriya1606 <83779316+SKpriya1606@users.noreply.github.com>
2021-05-10 12:05:12 +0530

    Add files via upload

---
+++ JQuery-DAY-1_8-5-21/ADM003-JQquery/js/index.js
@@ -0,0 +1,30 @@
+
+// $(document).ready(function(){
+//     $('button').click(()=>{
+//         $('p').addClass('add-color');
+//     });
+    
+// });
+// $(document).ready(function(){
+//     $('button').click(()=>{
+//         $('p').hide();
+//     });
+    
+// });
+
+// $(()=>{
+//     $('#hide').click(()=>{
+//         $("p").hide();
+//     });
+// });
+
+// $(()=>{
+//     $('#show').click(()=>{
+//         $("p").show();
+//     });
+// });
+    $(()=>{
+        $('button').click(()=>{
+            $("p").toggle();
+        });
+    });
